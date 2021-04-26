b2b976f0c919463c82d3cad8a69bc1056c55e57b
qiaoshuai0914 <qiaoshuai0914@163.com>
2021-04-26 22:40:07 +0800

    message

---
+++ src/main/java/com/qiaoshuai/linked/list/MoveZeroes.java
@@ -0,0 +1,27 @@
+package com.qiaoshuai.linked.list;
+
+public class MoveZeroes {
+    /**
+     * 283. 移动零
+     * 双指针的方法；
+     * i是循环指针
+     * j是第一个0； 也就是后面当i 是0时候需要跟j交换；所以j是第一个0
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i == j) {
+                    j++;
+                }else {
+                    nums[j] = nums[i];
+                    nums[i]=0;
+                    j++;
+                }
+
+            }
+
+        }
+    }
+}
